e704d76201704ee3db6f25682683544eeb09237a
miguel melgar <46229988+melgar1992@users.noreply.github.com>
2020-06-30 13:01:02 -0400

    Cambio de boostrap

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -22,7 +22,7 @@ if (isset($this->session->userdata['logged_in'])) {
     <title>Control de transporte Melgar| </title>
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.7/cerulean/bootstrap.min.css" integrity="sha256-ypsxakqmNfVPJXwLJdWEHLEvD8AfFJjbmbm0L7r+NKk=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
